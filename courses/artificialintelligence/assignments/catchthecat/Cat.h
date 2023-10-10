30a2790e01bcdd8c6a80912c53653d5c28c9cc4a
Jake Baranowski <jake.baranowski@mymail.champlain.edu>
2023-10-10 12:32:34 -0400

    A* is a corrosive agent

--- courses/artificialintelligence/assignments/catchthecat/Cat.h
+++ courses/artificialintelligence/assignments/catchthecat/Cat.h
@@ -7,5 +7,40 @@ struct Cat : public IAgent {
 
     return {0,0}; // todo: change this
   }
+
+  int GetNeighbors(const std::vector<bool>& world, std::pair<int,int> point, int sideSize ) {
+
+    std::pair<int,int> NorthWest = {0, 0};
+    std::pair<int,int> sideOne =  {0, 0};
+    std::pair<int,int> sideTwo =  {0, 0};
+    std::pair<int,int> sideThree =  {0, 0};
+    std::pair<int,int> sideFour =  {0, 0};
+    std::pair<int,int> sideFive =  {0, 0};
+
+    if (point.second % 2 == 0) {
+      NorthWest = {point.first, point.second - 1};  //Rename for
+      sideOne = {point.first , point.second - 1};
+      sideTwo = {point.first + 1, point.second};
+      sideThree = {point.first, point.second + 1};
+      sideFour = {point.first - 1, point.second + 1};
+      sideFive = {point.first - 1, point.second};
+    }
+    else
+    {
+      NorthWest = {point.first, point.second - 1};
+      sideOne = {point.first + 1, point.second - 1};
+      sideTwo = {point.first + 1, point.second};
+      sideThree = {point.first + 1, point.second + 1};
+      sideFour = {point.first, point.second + 1};
+      sideFive = {point.first - 1, point.second};
+    }
+
+    auto index = point.second * sideSize + point.first;
+    auto sideSquared = sideSize * sideSize;
+    if (index >= sideSquared) index %= sideSquared;
+    return world[currentBufferId % 2][index];
+
+    return world.Get(NorthWest) + world.Get(sideOne) + world.Get(sideTwo) + world.Get(sideThree) + world.Get(sideFour) + world.Get(sideFive);
+  }
 };
 #endif
