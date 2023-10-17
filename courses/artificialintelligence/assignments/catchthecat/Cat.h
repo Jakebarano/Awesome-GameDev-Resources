375896f212977bc7684ff70df7c7433be46cbbf5
Jake Baranowski <jake.baranowski@mymail.champlain.edu>
2023-10-17 14:17:09 -0400

    Started Transitioning CTC from Mobagen to AGDR
    Started working on adding Catcher logic
    Still need to add Cat Logic.

--- courses/artificialintelligence/assignments/catchthecat/Cat.h
+++ courses/artificialintelligence/assignments/catchthecat/Cat.h
@@ -1,46 +1,15 @@
 #ifndef CAT_h
 #define CAT_h
 #include "IAgent.h"
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
 
 struct Cat : public IAgent {
   std::pair<int,int> move(const std::vector<bool>& world, std::pair<int,int> catPos, int sideSize ) override{
 
     return {0,0}; // todo: change this
-  }
 
-  int GetNeighbors(const std::vector<bool>& world, std::pair<int,int> point, int sideSize ) {
-
-    std::pair<int,int> NorthWest = {0, 0};
-    std::pair<int,int> sideOne =  {0, 0};
-    std::pair<int,int> sideTwo =  {0, 0};
-    std::pair<int,int> sideThree =  {0, 0};
-    std::pair<int,int> sideFour =  {0, 0};
-    std::pair<int,int> sideFive =  {0, 0};
-
-    if (point.second % 2 == 0) {
-      NorthWest = {point.first, point.second - 1};  //Rename for
-      sideOne = {point.first , point.second - 1};
-      sideTwo = {point.first + 1, point.second};
-      sideThree = {point.first, point.second + 1};
-      sideFour = {point.first - 1, point.second + 1};
-      sideFive = {point.first - 1, point.second};
-    }
-    else
-    {
-      NorthWest = {point.first, point.second - 1};
-      sideOne = {point.first + 1, point.second - 1};
-      sideTwo = {point.first + 1, point.second};
-      sideThree = {point.first + 1, point.second + 1};
-      sideFour = {point.first, point.second + 1};
-      sideFive = {point.first - 1, point.second};
-    }
-
-    auto index = point.second * sideSize + point.first;
-    auto sideSquared = sideSize * sideSize;
-    if (index >= sideSquared) index %= sideSquared;
-    return world[currentBufferId % 2][index];
-
-    return world.Get(NorthWest) + world.Get(sideOne) + world.Get(sideTwo) + world.Get(sideThree) + world.Get(sideFour) + world.Get(sideFive);
-  }
 };
 #endif
--- courses/artificialintelligence/assignments/catchthecat/Catcher.h
+++ courses/artificialintelligence/assignments/catchthecat/Catcher.h
@@ -1,9 +1,106 @@
 #ifndef CATCHER_H
 #define CATCHER_H
 #include "IAgent.h"
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
 
 struct Catcher : public IAgent {
   std::pair<int,int> move(const std::vector<bool>& world, std::pair<int,int> catPos, int sideSize ) override{
+
+    // source cat
+    // destination exitPoint
+    // camefrom, set of elements from frontier, queue frontier, visited;
+
+
+    //TODO: Change ALL POINT2D to std::pair<int,int> (will be more efficient than making a Point2D struct.
+    //TODO: FIX OTHER ISSUES
+
+    unordered_map<Point2D, Point2D> cameFrom;
+    unordered_set<Point2D> frontierSet;
+    queue<Point2D> frontier;
+    unordered_map<Point2D, bool> visited;
+    Point2D current;
+
+    //auto cPos = w->getCat();   //Not needed
+    frontier.push(catPos);
+    frontierSet.insert(catPos);
+    Point2D exitPoint = Point2D(INT_MAX, INT_MAX);
+
+    while (!frontier.empty() && exitPoint == Point2D(INT_MAX, INT_MAX))
+    {
+      current = frontier.front();
+      frontier.pop();
+      frontierSet.erase(current);
+      visited[current] = true;
+
+      //const vector<Point2D> neighbors = w->neighbors(current);  //gonna need a simple neighbors function
+
+      for (auto next : neighbors) {
+        if (!visited[next] && next != catPos && !w->getContent(next) && !frontierSet.contains(next))
+        {
+          frontier.push(next);
+          frontierSet.insert(next);
+          cameFrom[next] = current;
+
+          if(w->catWinsOnSpace(next))
+          {
+            exitPoint = next;
+            break;
+          }
+        }
+      }
+    }
+
+    if (exitPoint == Point2D(INT_MAX, INT_MAX)) {
+      return {};  // Return an empty path
+    }
+
+    vector<Point2D> path;   //compile path form cameFrom and return it.
+
+    current = exitPoint;
+
+    while (current != catPos)
+    {
+      path.push_back(current);
+      current = cameFrom[current];
+    }
+    //return path;  //Remove Later.
+    //// Do Not Return Path, fix code below this to determine movement based off path after the while  and return it as a std::pair<int, int>.
+      if (!path.empty())
+      {
+        return path[0];
+      }
+      else {
+
+        auto pos = world->getCat();
+        auto neighbors = world->neighbors(pos);
+        std::unordered_set<Point2D> availableNeighbors (neighbors.begin(), neighbors.end());
+
+        while(!availableNeighbors.empty())
+        {
+          for(auto next : neighbors)
+          {
+            if(world->getContent(next))
+            {
+              availableNeighbors.erase(next);
+            }
+          }
+
+          auto rand = Random::Range(0, availableNeighbors.size()-1);
+
+          for(auto it = availableNeighbors.begin(); it != availableNeighbors.end(); it++)
+          {
+            if (rand == 0)
+            {
+              return *it;
+            }
+            rand--;
+          }
+        }
+      }
+    }
 
 
     return {0,0}; // todo: change this
