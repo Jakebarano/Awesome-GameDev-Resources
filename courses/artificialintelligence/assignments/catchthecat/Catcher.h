83fc642db841e814a8195f1db3514ff94ded8400
Jake Baranowski <jake.baranowski@mymail.champlain.edu>
2023-10-24 13:07:59 -0400

    namespace std works???
    an attempt to push in order to hopefully fix the namespace issue.

--- courses/artificialintelligence/assignments/catchthecat/Catcher.h
+++ courses/artificialintelligence/assignments/catchthecat/Catcher.h
@@ -7,24 +7,40 @@
 #include <unordered_map>
 #include <queue>
 
-using  namespace std;
+using namespace std;
 
 struct Point2D {
-  int32_t x; int32_t y;
+  int32_t x, y;
   Point2D(int32_t x, int32_t y): x(x), y(y) {}
-  Point2D(const std::pair<int32_t, int32_t>& p): x(p.first), y(p.second) {}
-
-  uint64_t hash() const noexcept { return ((uint64_t))
-
-
-
+  // for unordered_set unordered_map
+  uint64_t hash() const noexcept { return ((uint64_t)x) << 32 | (uint64_t)y; }
+  // for priority_queue, map, set
+  bool operator < (const Point2D& p) const{ return x < p.x || (x == p.x && y < p.y); }
+  // converts pair<int, int> to Point2D
+  Point2D(const pair<int32_t, int32_t>& p): x(p.first), y(p.second) {}
+  // converts Point2D to pair<int, int>
+  std::pair<int,int> toPair(){return {x,y};}
+  // add other operators, acessors, etc. ex.: NE(), NW(), SE(), SW(), E(), W()
+};
+// tell the compiler that we want to use Point2D as a key in unordered_set and unordered_map
+template <> struct std::hash<Point2D> {
+  std::size_t operator()(const Point2D& p) const noexcept { return p.hash(); }
+};
+struct AStarNode {
+  Point2D pos;
+  float accumulatedCost;
+  float heuristic;
+  // for priority_queue
+  bool operator < (const AStarNode& n) const{
+    return this->accumulatedCost + this->heuristic < n.accumulatedCost + n.heuristic;
+  }
 };
 
-namespace std {
-  template <> struct hash<Point2D> {
-    std::size_t operator()(const Point2D& p) const noexcept { return p.hash(); }
-  };
-}
+//namespace std {
+//  template <> struct hash<Point2D> {
+//    std::size_t operator()(const Point2D& p) const noexcept { return p.hash(); }
+//  };
+//}
 
 
 struct Catcher : public IAgent {
